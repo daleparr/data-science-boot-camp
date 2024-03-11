2b480917261c0e1ec41023e0609e97b79a4df5d9
Dale Parr <44434270+daleparr@users.noreply.github.com>
2024-03-11 10:54:53 +0000

    Delete Task 8 directory

--- Task 8/explorer.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-base_url = "https://www.farfetch.com/uk/"
-
-responce = requests.get(base_url)
-
-print("Your guide to shopping and placing orders" in responce.text)
